ce882e86770153af2251469355f38f7d5039559f
Joachim Hagege <joachim.h@sealsecurity.io>
2025-04-11 00:06:56 +0300

    Right constants for Intercom and GA

--- frontend/src/constants.tsx
+++ frontend/src/constants.tsx
@@ -1,5 +1,5 @@
-export const GOOGLE_ANALYTICS_ID = "G-LGHB46ZGWR";
-export const INTERCOM_ID = "rx71g1uo";
+export const GOOGLE_ANALYTICS_ID = "G-MS2EVEMLEQ";
+export const INTERCOM_ID = "jdztqca8";
 // TODO: Populate with your own Sentry DSN:
 // https://docs.sentry.io/product/sentry-basics/concepts/dsn-explainer/
 export const SENTRY_DSN: string | undefined = undefined;
